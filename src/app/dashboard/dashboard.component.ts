9ec4c820efeebbce02644ee925a9a4fdd3d9d6ad
sadatrafsanjani <sadatrafsanjani@gmail.com>
2020-06-09 20:13:14 +0600

    actuator service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../service/authentication.service';
 import {Router} from '@angular/router';
+import {ActuatorService} from '../service/actuator.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {HealthResponse} from '../response/health-response';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +12,35 @@ import {Router} from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+  private health: HealthResponse;
+
   constructor(private authenticationService: AuthenticationService,
-              private router: Router) { }
+              private actuatorService: ActuatorService,
+              private router: Router,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     if (!this.authenticationService.isAdmin()){
       this.router.navigateByUrl('/home');
     }
+
+    this.getHealth();
+  }
+
+  getHealth(){
+
+    this.spinner.show();
+    this.actuatorService.health().subscribe((data) => {
+
+      /*this.health.systemStatus = data.status;
+      this.health.databaseStatus = data.components.db.status;
+      this.health.databaseName = data.components.db.status;*/
+
+      console.log(this.health);
+
+      this.spinner.hide();
+    });
+
   }
 
 }
---
+++ src/app/response/health-response.ts
@@ -0,0 +1,10 @@
+export class HealthResponse {
+
+    systemStatus: string;
+    databaseStatus: string;
+    databaseName: string;
+    diskStatus: string;
+    diskTotal: string;
+    diskFree: string;
+    diskThreshold: string;
+}
