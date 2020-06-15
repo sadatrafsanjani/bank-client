37fa9a0cbc4918d560b4d9f8aa46263da3930386
sadatrafsanjani <sadatrafsanjani@gmail.com>
2020-06-15 16:23:12 +0600

    reset password front-end

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {AuthenticationService} from '../service/authentication.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgetPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgetForm: FormGroup;
+  errorFlag: boolean;
+
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router,
+              private toastr: ToastrService,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
   }
 
+  get email() {
+    return this.forgetForm.get('email');
+  }
+
+  onSubmit(){}
 }
